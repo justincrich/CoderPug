16228c2c0cda5d5968c7bb1513c9a2d861420ebb
Justin Rich <justinrich2008@gmail.com>
2017-06-17 17:09:37 -0700

    added some SCSS mixins, and started building out the about me section

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
 $(document).ready(()=>{
-  console.log("READY!");
+
+  var isActive = false;
+
+  $("#menuToggle").bind("click",()=>{
+
+    $('.bar').toggleClass('animate');
+
+    if(!isActive){
+       $('.navBody').css("height","100%");
+      $('.menu').css("display","block");
+      isActive= !isActive;
+    }else{
+      $('.navBody').css("height","60px");
+      isActive= !isActive;
+    }
+
+  });
 });
