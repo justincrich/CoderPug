b678ae94d3b648c426e187963a5c34abd57be6ae
Justin Rich <justinrich2008@gmail.com>
2017-08-14 21:24:04 -0700

    changed projects to be static

--- js/app.js
+++ js/app.js
@@ -4,30 +4,67 @@ $(document).ready(()=>{
     console.log("CLICK");
   });
 
-  var projects = [];
+  var rejProj = {
+    91938599:'reject'
+  };
+  var projFilter = [];
 
   // Fetch all github projects
-  fetch("https://api.github.com/users/justincrich/repos")
-  .then(resp=>resp.json())
-  .then(projects=>{
-    for(i in projects){
-      console.log(projects[i]);
-      let project = projects[i];
-      let tag =
-            '<div class=\'projectCard\'>'+
-								'<a target="_blank" href="'+project.html_url+'">'+
-										'<h2>'+project.name+'</h2>'+
-								'</a>'+
-								'<p>'+
-										project.description
-								'</p>'+
-						'</div>';
-      if(project.name !="justincrich.github.com"){
-        $('#projects .group').append(tag);
-      }
+  // fetch("https://api.github.com/users/justincrich/repos")
+  // .then(resp=>resp.json())
+  // .then(projects=>{
+  //   for(i in projects){
+  //     console.log(projects[i]);
+  //     let project = projects[i];
+  //
+  //     if((!rejProj[project.id])){
+  //       if(project.has_pages){
+  //         console.log(project.owner.id);
+  //         fetch("https://api.github.com/repos/"+project.owner.login+"/"+project.id+"/pages")
+  //         .then(resp=>resp.json())
+  //         .then(siteData =>{
+  //           console.log(siteData);
+  //         });
+  //       }else{
+  //         listProj(project);
+  //       }
+  //     }
+  //
+  //   }
+  // });
 
-    }
-  });
+  // function listProj(project){
+  //
+  //   let codeURL = project.url;
+  //   let projSite = document.createElement('div');
+  //   projSite.innerHTML = '<a target="_blank" href="#">'+
+  //       '<li>'+
+  //           '<i class="fa fa-globe" aria-hidden="true"></i>'+
+  //       '</li>'+
+  //   '</a>';
+  //   let projCode = document.createElement('div');
+  //   projCode.innerHTML = '<a target="_blank" href="#">'+
+  //       '<li>'+
+  //           '<i class="fa fa-code" aria-hidden="true"></i>'+
+  //       '</li>'+
+  //   '</a>';
+  //
+  //   let tag =
+  //         '<div class=\'projectCard\'>'+
+  //             '<div class="projHeader">'+
+  //               '<h2>'+project.name+'</h2>'+
+  //               '<ul class="projLinkContainer">'+
+  //               '</ul>'+
+  //             '</div>'+
+  //             '<p>'+
+  //                 project.description+
+  //             '</p>'+
+  //         '</div>';
+  //         console.log(tag);
+  //   if(project.name !="justincrich.github.com"){
+  //     $('#projects .group').append(tag);
+  //   }
+  // }
 
   $("header").scrollspy({offset:-80});
 
@@ -47,7 +84,7 @@ $(document).ready(()=>{
     }
 
   });
-  
+
 
 
 });
